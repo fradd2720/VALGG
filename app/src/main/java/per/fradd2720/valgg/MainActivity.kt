e094cabe74673ccbaa14d1130a70e4cb19a30839
fradd27 <fradd2720@naver.com>
2022-09-09 12:23:13 +0900

    add riot.txt

--- app/src/main/java/per/fradd2720/valgg/MainActivity.kt
+++ app/src/main/java/per/fradd2720/valgg/MainActivity.kt
@@ -1,6 +1,7 @@
 package per.fradd2720.valgg
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -35,6 +36,8 @@ class MainActivity : AppCompatActivity() {
                 .setAnchorView(R.id.fab)
                 .setAction("Action", null).show()
         }
+
+        Log.d("fradd1503", BuildConfig.SAMPLE_API_KEY);
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
